8098bbe8e04c8b81711557df90277815cda04de1
Juuso154 <s2300895@edu.bc.fi>
2025-10-02 12:01:13 +0300

    Marketplace ja upgradet

--- Assets/Scripts/MarketPlace.cs
+++ Assets/Scripts/MarketPlace.cs
@@ -2,15 +2,76 @@ using UnityEngine;
 
 public class MarketPlace : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject kauppaCanvas;
+
+    // napit ja hinnat
+    [SerializeField] private int kissaCost = 10;
+    [SerializeField] private int autoCost = 50;
+    [SerializeField] private int mukiCost = 20;
+    [SerializeField] private int palmuCost = 5;
+
+    private MoneySystem moneySystem;
+
+    private bool kissaBought = false;
+    private bool autoBought = false;
+    private bool mukiBought = false;
+    private bool palmuBought = false;
+
+    private void Start()
     {
-        
+        //moneySystem = FindObjectOfType<MoneySystem>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadKauppa()
     {
-        
+        kauppaCanvas.SetActive(true);
+    }
+
+    public void ExitKauppa()
+    {
+        kauppaCanvas.SetActive(false);
+    }
+
+    // kissa upgrade tuo passiivista tuottoa
+    public void BuyKissa()
+    {
+        if (!kissaBought && moneySystem.SpendMoney(kissaCost))
+        {
+            kissaBought = true;
+            moneySystem.AddPassiveIncome(0.2f); 
+            
+        }
+    }
+
+    // auto upgarde antaa crash shieldin
+    public void BuyAuto()
+    {
+        if (!autoBought && moneySystem.SpendMoney(autoCost))
+        {
+            autoBought = true;
+            moneySystem.EnableCrashShield(0.1f);
+            Debug.Log("Ostit Auton");
+        }
+    }
+
+    // 1.2x profit multiplier
+    public void BuyMuki()
+    {
+        if (!mukiBought && moneySystem.SpendMoney(mukiCost))
+        {
+            mukiBought = true;
+            moneySystem.SetProfitMultiplier(1.2f); // 1.2x profits
+            Debug.Log("Ostit Muki");
+        }
+    }
+
+    // kosmeettinen esine
+    public void BuyPalmu()
+    {
+        if (!palmuBought && moneySystem.SpendMoney(palmuCost))
+        {
+            palmuBought = true;
+            Debug.Log("Ostit Palmun");
+        }
     }
 }
--- Assets/Scripts/MoneySystem.cs
+++ Assets/Scripts/MoneySystem.cs
@@ -3,12 +3,34 @@ using UnityEngine.UI;
 
 public class MoneySystem : MonoBehaviour
 {
-    public int money = 100;         // Pelaajan käteinen alussa
-    public Text moneyText;         // UI-kenttä inspectorissa
+    public int money = 100;          // Pelaajan käteinen alussa
+    public Text moneyText;
+
+    public float profitMultiplier = 1f;   // Alussa 1.0x
+    public bool hasCrashShield = false;   // Onko crash shield ostettu
+    private float crashShieldStrength = 0f;
+
+    private float passiveIncome = 0f;     // $/sekunti
+    private float passiveTimer = 0f;
 
     void Start()
     {
         UpdateMoneyUI();
+    }
+
+    void Update()
+    {
+        // passiivitulo laskuri
+        if (passiveIncome > 0)
+        {
+            passiveTimer += Time.deltaTime;
+            if (passiveTimer >= 1f)
+            {
+                money += Mathf.RoundToInt(passiveIncome);
+                passiveTimer = 0f;
+                UpdateMoneyUI();
+            }
+        }
     }
 
     public bool CanSpend(int amount)
@@ -18,7 +40,7 @@ public class MoneySystem : MonoBehaviour
 
     public void AddMoney(int amount)
     {
-        money += amount;
+        money += Mathf.RoundToInt(amount * profitMultiplier);
         UpdateMoneyUI();
     }
 
@@ -31,6 +53,27 @@ public class MoneySystem : MonoBehaviour
             return true;
         }
         return false;
+    }
+
+    public void AddPassiveIncome(float amount)
+    {
+        passiveIncome += amount;
+    }
+
+    public void SetProfitMultiplier(float multiplier)
+    {
+        profitMultiplier = multiplier;
+    }
+
+    public void EnableCrashShield(float strength)
+    {
+        hasCrashShield = true;
+        crashShieldStrength = strength; // esim. 0.1 = 10%
+    }
+
+    public float GetCrashShieldStrength()
+    {
+        return crashShieldStrength;
     }
 
     void UpdateMoneyUI()
