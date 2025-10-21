baff052a9f5987779e706e7625b52a35ef93f72e
Para-cpu <s2300889@edu.bc.fi>
2025-10-21 12:50:17 +0300

    Money debug

---
+++ Assets/Scripts/MoneyDebug.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MoneyDebug : MonoBehaviour
+{
+    private MoneySystem moneySystem;
+
+    void Start()
+    {
+        moneySystem = FindObjectOfType<MoneySystem>();
+    }
+
+    public void AddDebugMoney()
+    {
+        if (moneySystem != null)
+        {
+            moneySystem.AddMoney(100);
+            Debug.Log("Added $100 via Debug Button");
+        }
+    }
+}
